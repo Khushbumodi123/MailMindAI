e6247b2ea814a5c210e37257b34b913021f9fb6c
Khushbu <0109khushbu@gmail.com>
2025-04-04 18:58:24 -0400

    fix:services folder

--- src/main/java/com/email/writer/models/EmailRequest.java
+++ src/main/java/com/email/writer/models/EmailRequest.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class EmailRequest{
-    private String emailContent;
+    private String Content;
     private String tone;
 }
--- src/main/java/com/email/writer/services/EmailGeneratorService.java
+++ src/main/java/com/email/writer/services/EmailGeneratorService.java
@@ -3,6 +3,11 @@ package com.email.writer.services;
 import org.springframework.stereotype.Service;
 import com.email.writer.models.EmailRequest;
 import com.email.writer.helpers.BuildPrompt;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Service
 public class EmailGeneratorService {
@@ -13,9 +18,12 @@ public class EmailGeneratorService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
+    @Autowired
+    private BuildPrompt buildPrompt;
+
     public String generateEmail(EmailRequest emailRequest) {
         //Build a prompt
-        String prompt = buildPrompt(emailRequest);
+        String prompt = buildPrompt.buildPrompt(emailRequest);
         // Craft a request to Gemini API
         Map<String, Object> requestBody = Map.of(
             "contents",new Object[] {
@@ -27,7 +35,7 @@ public class EmailGeneratorService {
                     }
                 )
             }
-        )
+        );
         // Send the request to Gemini API
         // Get the response from Gemini API
         // Return the response
