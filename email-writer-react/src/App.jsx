97a99c33c1ad70027b15e58c2926d666c07b9b76
Khushbu <0109khushbu@gmail.com>
2025-04-10 22:33:11 -0400

    fix:main react page

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [emailContent, setEmailContent] = useState('');
+  // const [tone, setTone] = useState('');
+  // const [generatedReply, setGeneratedReply] = useState('');
+  // const [loding, setLoading] = useState(false);
+  // const [error, setError] = useState('');
+
   return (
-    <>
-    </>
+    <Container maxWidth="md" sx={{ py:4 }}>
+      <Typography variant="h4" component="h1" align="center" gutterBottom>
+        Email Reply Generator
+      </Typography>
+      <Box sx={{ mx: 3 }}>
+        <TextField
+          fullWidth
+          Multiline
+          rows={6}
+          label="Original Email Content"
+          variant="outlined"
+          value={emailContent || ''}
+          onChange={(e) => setEmailContent(e.target.value)}
+          sx={{ mb: 2 }}
+        />
+        </Box>
+    </Container>
   )
 }
 
